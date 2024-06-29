052ee92c91484a54bafcc7bf4e9ff8f2cbf398cd
BoyaRaju546 <167398154+BoyaRaju546@users.noreply.github.com>
2024-06-29 16:30:33 +0530

    Add files via upload

---
+++ countdown_timer/script.js
@@ -0,0 +1,34 @@
+function startCountdown() {
+    const userDateTime = document.getElementById('datetime').value;
+    const countdownDate = new Date(userDateTime).getTime();
+
+    
+    const countdownInterval = setInterval(function () {
+        
+        const now = new Date().getTime();
+
+        
+        const distance = countdownDate - now;
+
+    
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        const countdownElement = document.getElementById('countdown');
+        countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        
+        if (distance < 0) {
+            clearInterval(countdownInterval);
+            countdownElement.innerHTML = "Time up!";
+        }
+    }, 1000);
+}
+function gta(){
+    const countdownElement = document.getElementById('countdown');
+    countdownElement.innerHTML = "";
+    document.getElementById('datetime').value = "";
+    clearInterval(countdownInterval);
+}
